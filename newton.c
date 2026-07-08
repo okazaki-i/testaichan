ce492789cac0cc67979276ab900207dce54fad91
okazaki,i <none@kalmia>
2026-07-08 12:36:33 +0900

    update newton.c, temporary

--- newton.c
+++ newton.c
@@ -3,46 +3,73 @@
 #include  <stdlib.h>
 #include  <math.h>
 
-double newton( double x );
-double f( double x );
-double df( double x );
+double newton( double v, double x0, double threshold, int maxloop ); //前方宣言
+double f( double x, double v );
+double df( double x, double v );
 
 int main( void )
 {
-    double  x0, x;
+    double  x, v;
+    double  dv = 0.5/1000;
 
-    x0 = 1.0;
-
-    x = newton( x0 );
-    printf( "solution = %.15f\n", x );
-
+    for ( v=0.0/1000; v<20.0/1000+dv/2; v+= dv ) {
+        x = newton( v, 1e+12, 1e-16, 500 ); //滴定量v、初期値、収束判定のしきい値、繰返し最大回数
+        printf( "v= %.4lf L : solution = %25.15le %6.3lf\n", v, x, -log10(x) );
+    }
     return 0;
 }
 
-double newton( double x )
+double newton( double v, double x0, double threshold, int maxloop )
 {
     int     i;
-    double  xnew, dx;
+    double  x, dx;
 
-    for ( i = 1; i <= 30; i++ ) {
-
-        dx = - f(x) / df(x);
+//    printf( "#v=%.15le\n", v );
+//    printf( "#%4s : %25s  %25s\n", "iter", "x", "dx" );
+    for ( x=x0, i=0; i < maxloop; i++ ) {
+        dx = - f(x,v) / df(x,v);
         x = x + dx;
-
-        printf( "%2d : %.15lf  %.15lf\n", i, x, dx );
-
-        if ( fabs( dx ) < 1e-12 ) break;
+//        printf( " %4d : %25.15le  %25.15le\n", i, x, dx );
+        if ( fabs( dx ) < threshold ) break;
+    }
+    printf( " %4d : %25.15le  %25.15le    ", i, x, dx );
+    if ( i == maxloop ) {
+        fprintf( stderr, "not converged\n" );
+        exit( 1 );
     }
 
     return x;
 }
 
-double f( double x )
+double f( double x, double v )
 {
-    return x*x-2.0;
+    double  v0=10.0/1000, m0=0.1, m=0.1, ka=1.74e-5, kw=1e-14;
+//    return (v0+v)*x*x + ( (v0+v)*ka + m*v )*x + (m*v-m0*v0)*ka - (v0+v)*kw - (v0+v)*kw*ka/x;
+    return (v0+v)*x*x*x + ( (v0+v)*ka + m*v )*x*x + ( (m*v-m0*v0)*ka - (v0+v)*kw )*x - (v0+v)*kw*ka;
+//    return x*x-2.0;
+//    return cos(x)-x;
+//    return exp(-x)-x;
 }
 
-double df( double x )
+double df( double x, double v )
 {
-    return 2.0*x;
+    double  v0=10.0/1000, m0=0.1, m=0.1, ka=1.74e-5, kw=1e-14;
+//    return 2.0*(v0+v)*x + ( (v0+v)*ka + m*v ) + (v0+v)*kw*ka/x/x;
+    return 3.0*(v0+v)*x*x + 2.0*( (v0+v)*ka + m*v )*x + ( (m*v-m0*v0)*ka - (v0+v)*kw );
+//    return 2.0*x;
+//    return -sin(x)-1.0;
+//    return -exp(x)-1.0;
 }
+
+
+/*
+
+v0=10.0/1000, m0=0.1, m=0.1, ka=1.74e-5, kw=1e-14;
+
+f(x,v)= (v0+v)*x*x*x + ( (v0+v)*ka + m*v )*x*x + ( (m*v-m0*v0)*ka - (v0+v)*kw )*x - (v0+v)*kw*ka
+finv(x,v)=(v0+v)*x*x + ( (v0+v)*ka + m*v )*x + (m*v-m0*v0)*ka - (v0+v)*kw - (v0+v)*kw*ka/x
+
+plot for [v in "0 5 10 15 20"] f(x,v/1000.0) title sprintf("v=%s",v)
+plot for [v in "0 5 10 15 20"] finv(x,v/1000.0) title sprintf("v=%s",v)
+
+*/
